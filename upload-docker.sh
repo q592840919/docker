8168b34c38e16a9045e64f1d5d7ac42df1c7e4d6
q592840919 <xuefengmsa@163.com>
2018-05-25 14:23:38 +0800

    2018-05-25 14:23:38

--- upload-docker.sh
+++ upload-docker.sh
@@ -7,5 +7,5 @@ git add -A
 git commit -m "$v_time"
 git push origin master
 echo "success push git!"
-ssh root@47.95.215.144 "sh /shells/rebase-shells.sh"
+ssh root@47.95.215.144 "sh /shells/rebase-docker.sh"
 echo "gone1"
